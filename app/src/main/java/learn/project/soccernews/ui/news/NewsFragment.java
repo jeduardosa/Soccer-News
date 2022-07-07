57803e974e17b5d0f2402fe4fd5c49ddf416be75
jeduardosa <jeduardosa86@gmail.com>
2022-07-07 08:08:05 -0300

    Project replace

--- app/src/main/java/learn/project/soccernews/ui/news/NewsFragment.java
+++ app/src/main/java/learn/project/soccernews/ui/news/NewsFragment.java
@@ -5,7 +5,7 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.TextView;
-<<<<<<< HEAD
+
 import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
 import androidx.lifecycle.ViewModelProvider;
@@ -14,36 +14,22 @@ import androidx.recyclerview.widget.LinearLayoutManager;
 import learn.project.soccernews.databinding.FragmentNewsBinding;
 import learn.project.soccernews.ui.adapter.NewsAdapter;
 
-=======
-
-import androidx.annotation.NonNull;
-import androidx.fragment.app.Fragment;
-import androidx.lifecycle.ViewModelProvider;
-
-import learn.project.soccernews.databinding.FragmentNewsBinding;
->>>>>>> 06e290e1dd7ada5051a1805e5d6e4f635dfe7790
 
 public class NewsFragment extends Fragment {
 
     private FragmentNewsBinding binding;
 
-    public View onCreateView(@NonNull LayoutInflater inflater,
-                             ViewGroup container, Bundle savedInstanceState) {
-        NewsViewModel newsViewModel =
-                new ViewModelProvider(this).get(NewsViewModel.class);
+    public View onCreateView(@NonNull LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        NewsViewModel newsViewModel = new ViewModelProvider(this).get(NewsViewModel.class);
 
         binding = FragmentNewsBinding.inflate(inflater, container, false);
         View root = binding.getRoot();
 
-<<<<<<< HEAD
         binding.rvNews.setLayoutManager(new LinearLayoutManager(getContext()));
         newsViewModel.getNews().observe(getViewLifecycleOwner(), news -> {
             binding.rvNews.setAdapter(new NewsAdapter(news));
         });
-=======
-        final TextView textView = binding.textNews;
-        newsViewModel.getText().observe(getViewLifecycleOwner(), textView::setText);
->>>>>>> 06e290e1dd7ada5051a1805e5d6e4f635dfe7790
+
         return root;
     }
 
--- app/src/main/java/learn/project/soccernews/ui/news/NewsViewModel.java
+++ app/src/main/java/learn/project/soccernews/ui/news/NewsViewModel.java
@@ -3,11 +3,8 @@ package learn.project.soccernews.ui.news;
 import androidx.lifecycle.LiveData;
 import androidx.lifecycle.MutableLiveData;
 import androidx.lifecycle.ViewModel;
-
-<<<<<<< HEAD
 import java.util.ArrayList;
 import java.util.List;
-
 import learn.project.soccernews.domain.News;
 
 public class NewsViewModel extends ViewModel {
@@ -27,18 +24,6 @@ public class NewsViewModel extends ViewModel {
 
     public LiveData<List<News>> getNews() {
         return news;
-=======
-public class NewsViewModel extends ViewModel {
-
-    private final MutableLiveData<String> mText;
-
-    public NewsViewModel() {
-        mText = new MutableLiveData<>();
-        mText.setValue("This is home fragment");
     }
 
-    public LiveData<String> getText() {
-        return mText;
->>>>>>> 06e290e1dd7ada5051a1805e5d6e4f635dfe7790
-    }
 }
